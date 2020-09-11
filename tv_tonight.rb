b73bc0b76d80fc6b3937467fe8fc00c3c2c9a4df
Jordan Hayes <52760603+Hayes525@users.noreply.github.com>
2020-09-11 15:00:54 -0400

    Add files via upload

---
+++ tv_tonight.rb
@@ -0,0 +1,14 @@
+require "tv_tonight/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require relative "./tv_tonight/version"
+require relative './tv_tonight/episode'
+require relative './tv_tonight/cli'
+require relative './tv_tonight/scraper'
+
+#module TvTonight
+  #class Error < StandardError; end
+  # Your code goes here...
+#end
